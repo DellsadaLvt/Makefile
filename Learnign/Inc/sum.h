c85fa18bfcefd782cd0e9b3d20780e08368c57c1
DellsadaLvt <levantho0168@gmail.com>
2022-05-20 23:14:07 +0700

    update 20220520

---
+++ Learnign/Inc/sum.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <stdint.h>
+
+void sum( const uint8_t a, const uint8_t b );
---
+++ Learnign/Src/sum.c
@@ -0,0 +1,8 @@
+#include "sum.h"
+
+void sum( const uint8_t a, const uint8_t b ){
+	uint8_t ret = 0u;
+	
+	ret = a + b;
+	printf("The sum is: %d\n", ret);
+}
