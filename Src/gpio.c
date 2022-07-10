a27d916299170f77111cab8ed8079e1a86746fc9
DellsadaLvt <levantho0168@gmail.com>
2022-07-10 23:30:02 +0700

    Temp_2

---
+++ Src/gpio.c
@@ -0,0 +1,19 @@
+#include "stm32f10x.h"
+
+void gpio_init_pc13(void)
+{
+    RCC->APB2ENR |= 0x10;	  /* Or  0b10000 --> Anabling Preiph GPIOC */
+    GPIOC->CRH &= 0xFF0FFFFF; /* Reset the PORT C PIN 13 */
+    GPIOC->CRH |= 0x00300000; /* Set Port C PIN 13 as Output  */
+    GPIOC->ODR |= 0x2000;	  /* Set Port C Pin 13 */
+}
+
+void gpio_set_pc13(void)
+{
+    GPIOC->ODR |= 0x2000;
+}
+
+void gpio_reset_pc13(void)
+{
+    GPIOC->ODR &= ~0x2000;
+}
