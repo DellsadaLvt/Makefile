404e3d135dbe88072f5fce4d50e7c248459a2b17
DellsadaLvt <levantho0168@gmail.com>
2022-07-10 18:19:11 +0700

    Mile stone 0

--- Src/main.c
+++ Src/main.c
@@ -1,9 +1,12 @@
 #include "sum.h"
+#include "sub.h"
 
 int main( void ){
 
 	printf("Hello\n");
 	sum(2u, 8u);
+	sumn(3u, 6u);
+	sub(6u, 3u);
 
 	return 0u;
 }
--- sub/subb.c
+++ sub/subb.c
@@ -4,5 +4,5 @@ void sub( const uint8_t a, const uint8_t b ){
 	uint8_t ret = 0u;
 
 	ret = a - b;
-	printf("The sum is: %d\n", ret);
+	printf("The sub is: %d\n", ret);
 }
